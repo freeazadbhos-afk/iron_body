5b9933400152c795ed579da8ed34295ab87aeb0d
freeazadbhos-afk <freeazadbhos@gmail.com>
2026-04-08 19:40:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8127,7 +8127,7 @@ function ProfileView({
           }}
         >
           IRON BODY{" "}
-          <span style={{ color: th.accentFg, fontWeight: 700 }}>v1.2.1</span>
+          <span style={{ color: th.accentFg, fontWeight: 700 }}>v1.2.2</span>
         </div>
         <div style={{ color: th.dim, fontSize: 11, letterSpacing: "2px" }}>
           DEVELOPED BY AZAD
