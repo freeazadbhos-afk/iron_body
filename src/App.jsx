b9bbbd95f24c0011141bf47334296e028c67b317
freeazadbhos-afk <freeazadbhos@gmail.com>
2026-04-14 23:41:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8410,7 +8410,7 @@ import "./styles.css";
             }}
           >
             IRON BODY{" "}
-            <span style={{ color: th.accentFg, fontWeight: 700 }}>v1.4.0 </span>
+            <span style={{ color: th.accentFg, fontWeight: 700 }}>v1.4.1 </span>
           </div>
           <div style={{ color: th.dim, fontSize: 11, letterSpacing: "2px" }}>
             DEVELOPED BY AZAD
